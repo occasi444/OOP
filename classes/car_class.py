b1409e30b30f8034c9762bfb0aec3405f51ca16b
Julian <julian.ms.braun@gmail.com>
2024-06-02 19:29:38 +0200

    einfache vererbung

--- classes/car_class.py
+++ classes/car_class.py
@@ -11,3 +11,15 @@ class Car:
 
     def stop(self):
         print("The " +self.model+ " has stopped")
+
+class Audi(Car):
+
+    def __init__ (self, make, model, year, color, speed):
+
+        super().__init__ (make, model, year, color)
+
+        self.speed = speed
+
+A6 = Audi("Audi", "A6", 2012, "silver", 270)
+
+print (A6.speed)
--- classes/tier_class.py
+++ classes/tier_class.py
@@ -9,3 +9,15 @@ class Tier:
 
         # self.alter wird umgewandelt und dan mit Punkt verkettet
         print("Der " +self.rasse+ " ist", str(self.alter) + ".")
+
+class Hund(Tier):
+
+    def __init__ (self, rasse, geschlecht, alter, farbe):
+        
+        super().__init__ (rasse, geschlecht, alter)
+
+        self.farbe = farbe
+
+Bello = Hund("labrador", "mänlich", 12, "braun")
+
+print (Bello.farbe)   
